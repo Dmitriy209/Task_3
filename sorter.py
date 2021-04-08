b969a76a0cae23f6c41baabc906485813f3bb8a7
dim286754 <dim286754@gmail.com>
2021-04-08 14:32:33 +0500

    Цикл создает директорию с именем исполнителя для каждого файла в указанной директории

--- sorter.py
+++ sorter.py
@@ -1,31 +1,44 @@
 import os
+import glob
 from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
+
 #Создание списка всех файлов в папке
 arr = os.listdir('D:\погромирование\Task_3\Подборка музыки')
 print(arr)
+#Цикл для переборки файлов в директории
+namedir=input("Введите директорию: ")
+os.chdir(namedir) #Перемещение в нужную директорию
+#Цикл создает директорию с именем исполнителя для каждого файла в указанной директории
+for j in range(len(arr)):
+    # Извлечение тегов
+    file = os.path.abspath(arr[j])
+    print(file)
+    mp3 = MP3File(file)
+    tags = mp3.get_tags()
+    ID3TagV1 = tags['ID3TagV1']
+    song = ID3TagV1['song']
+    artist = ID3TagV1['artist']
+    album = ID3TagV1['album']
+    #print('song ', song)
+    #print('artist ', artist)
+    #print('album ', album)
+    print(arr[j])
+    os.mkdir(artist) #Создание директории
+    j+=1
 
-mp3 = MP3File('D:\погромирование\Task_3\Подборка музыки\A Day To Remember - Since U Been Gone.mp3')
 
-tags = mp3.get_tags()
-print(tags)
-"""
-Output:
-{'ID3TagV1': {'song': 'Prowler', 'artist': 'Iron Maiden', 'album': 'Iron Maiden',
-'year': '1980', 'comment': None, 'track': 1, 'genre': 'Other'},
-'ID3TagV2': {'artist': 'Iron Maiden', 'band': 'Iron Maiden', 'album': 'Iron Maiden',
-'song': 'Prowler', 'track': '1/9', 'genre': 'Heavy Metal', 'year': '1980'}}
-"""
-#Извлечение музыки
-ID3TagV1=tags['ID3TagV1']
-song=ID3TagV1['song']
-artist=ID3TagV1['artist']
-album=ID3TagV1['album']
-print('song ', song)
-print('artist ', artist)
-print('album ', album)
+#print(tags)
+
 #Извлечение тегов в словарь
-dict={}
-dict1={song:1}
-print(dict)
-dict.update({song:{artist:album}})
-print(dict)
+#dict={}
+#dict1={song:1}
+#print(dict)
+#dict.update({song:{artist:album}})
+#print(dict)
+#print('os.walk')
+
+#попытка создания цикла для взятие имен файлов
+#for i in os.walk('D:\погромирование\Task_3\Подборка музыки'):
+#    print('Начало')
+#    print(i[2])
+#    print('Конец')
